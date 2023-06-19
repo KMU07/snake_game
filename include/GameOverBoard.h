031dfd4df908f24fdaa8347e349187164a614b8c
yubincho3 <yubincho3@naver.com>
2023-06-19 17:16:28 +0900

    Improve the GameOverBoard UI

--- include/GameOverBoard.h
+++ include/GameOverBoard.h
@@ -1,29 +1,13 @@
 #pragma once
-// #include "TextBox.h"
 #include <string>
-#include "GameObject.h"
+#include "TextBox.h"
 
-// 임시!
-class GameOverBoard : public GameObject
+class GameOverBoard : public TextBox
 {
+    const std::string deathMessage;
+
 public:
-    GameOverBoard(std::string txt)
-        : GameObject("TEMP", 0)
-    {
-        int x = 0;
-        for (char c : txt)
-        {
-            Pixel p;
+    GameOverBoard(std::string reasonOfDeath);
 
-            p.pos = Vector2Int(x++, 0);
-            p.ch = c;
-            pixels.push_back(p);
-        }
-    }
-
-    bool isCollisionable() const final
-    {
-        return false;
-    }
-
+    void updateText() final;
 };
